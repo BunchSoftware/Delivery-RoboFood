e851cc20b94e214db9633de51328d91eae37cd6e
Денис <94766316+DSD-Games@users.noreply.github.com>
2023-09-09 19:56:23 +0700

    0.12
    - Сделал колеса и управление ими

--- Delivery RoboFood/Assets/Scripts/Camera/Follower.cs
+++ Delivery RoboFood/Assets/Scripts/Camera/Follower.cs
@@ -9,13 +9,15 @@ public class Follower : MonoBehaviour
     [SerializeField] private Transform target; // Цель за которой следует камера
     [SerializeField] private float sensition; // Чуствительность мыши
     [SerializeField] private float smoothMouseMoveTime;
+    [Header("Смещение камеры")]
+    [SerializeField] private Vector2 offsetPositon;
+    [SerializeField] private Vector3 startOffsetRotation;
     private Vector3 euler; // Углы поворота
     private float mouseY = 0f; // Положение мыши по Y
     private float mouseX = 0f; // Положение мыши по X
-    private Vector3 velocityEuler;
 
     [Header("Настройка приближения камеры")]
-    [SerializeField] private float maxZoom; // Максимальное увелечение
+    [SerializeField] private float maxZoom = 8f; // Максимальное увелечение
     [SerializeField] private float smoothZoomTime = 0.35f;
     private float zoom; // Увелечение
     private float zoomVelocity = 0f; // Ось Увелечения
@@ -24,7 +26,16 @@ public class Follower : MonoBehaviour
 
     private void Start()
     {
-        euler = transform.localEulerAngles;
+        euler = new Vector3
+            (startOffsetRotation.x, 
+            startOffsetRotation.y, 
+            startOffsetRotation.z) ;
+
+        transform.rotation = Quaternion.Euler(euler);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+
+        mouseScrollDeltaY = maxZoom;
+        zoom = mouseScrollDeltaY;
     }
     private void LateUpdate()
     {
@@ -43,7 +54,7 @@ public class Follower : MonoBehaviour
 
         transform.position = Vector3.Lerp(transform.position, target.position, Time.fixedDeltaTime);
 
-        transform.Translate(new Vector3(0, 0, -zoom));
+        transform.Translate(new Vector3(offsetPositon.x, offsetPositon.y, -zoom));
     }
     private void MouseRotate()
     {
@@ -58,12 +69,24 @@ public class Follower : MonoBehaviour
 
         euler.y += mouseX;
 
-        euler.z = 0;
+        euler.z = startOffsetRotation.z;
 
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(euler), Time.fixedDeltaTime * sensition);
             transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        euler = new Vector3
+           (startOffsetRotation.x,
+           startOffsetRotation.y,
+           startOffsetRotation.z);
+
+        transform.rotation = Quaternion.Euler(euler);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+    }
+
 }
 
 
